bedb12882f0721ff4aba63e38c7bd97037340593
Steve Saunders <steve@map59.com>
2026-07-19 16:32:09 -1000

    Bump version

--- MQTT send receive.lua
+++ MQTT send receive.lua
@@ -25,7 +25,7 @@ Resident, zero sleep interval, name: 'MQTT send receive'
 Manage CBus, Panasonic and Airtopia AC and environment events for MQTT, and publish discovery topics. Used with Home Assistant.
 
 Documentation available at https://github.com/autoSteve/acMqtt
-Downloaded version: 1.0.0
+Downloaded version: 1.1.0
 --]]
 
 --[[
